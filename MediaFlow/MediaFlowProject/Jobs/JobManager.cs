0234a93f4a78a50d3a7b60e0667ae74a84817f08
Narek Srapionyan <n.srapionyan05@gmail.com>
2026-08-23 20:46:44 +0400

    Some changes

--- MediaFlow/MediaFlowProject/Jobs/JobManager.cs
+++ MediaFlow/MediaFlowProject/Jobs/JobManager.cs
@@ -1,6 +1,79 @@
-﻿namespace MediaFlowProject.Jobs;
+﻿using System.Collections.Concurrent;
+using MediaFlowProject.Models;
+
+namespace MediaFlowProject.Jobs;
 
 public class JobManager
 {
+    private readonly BlockingCollection<MediaJob> _jobQueue;
+    private readonly List<MediaJob> _alljobs;
+    private readonly object _listLock = new object();
+
+    private readonly int _workerCount;
+    private readonly List<Thread> _workers;
+
+    private readonly CancellationTokenSource _tokenSource;
+    
+    public JobManager(int workerCount)
+    {
+        _workerCount = Math.Clamp(workerCount,1,5);
+        _jobQueue = new BlockingCollection<MediaJob>();
+        _alljobs = new List<MediaJob>();
+        
+        _workers = new List<Thread>();
+        _tokenSource = new CancellationTokenSource();
+    }
+
+    public void AddJob(MediaJob job)
+    {
+        lock (_listLock)
+        {
+            _alljobs.Add(job);
+        }
+
+        if (!_jobQueue.IsAddingCompleted)
+        {
+            _jobQueue.Add(job);
+        }
+    } 
+    public List<MediaJob> GetJobs()
+    {
+        lock (_listLock)
+        {
+            return _alljobs.ToList(); 
+        }
+    }
+
+    public void StartWorkers()
+    {
+        for (int i = 0; i < _workerCount; i++)
+        {
+            var thread = new Thread(WorkerLoop)
+            {
+                Name = $"WorkerThread-{i + 1}",
+                IsBackground = true
+            };
+            _workers.Add(thread);
+            thread.Start();
+        }
+    }
+
+    public void StopWorkers()
+    {
+        _tokenSource.Cancel();
+        _jobQueue.CompleteAdding();
+        foreach (var worker in _workers)
+        {
+            worker.Join();
+        }
+    }
+
+    private void WorkerLoop()
+    {
+        // TODO:
+    }
+    
     
 }
+    
+    
