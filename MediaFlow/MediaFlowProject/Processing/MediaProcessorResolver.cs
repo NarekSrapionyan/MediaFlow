c7e3387d3b8c83515e2a6b46f7a9b6fee48fbcf6
Narek Srapionyan <n.srapionyan05@gmail.com>
2026-08-24 11:09:33 +0400

    Nice!

--- MediaFlow/MediaFlowProject/Processing/MediaProcessorResolver.cs
+++ MediaFlow/MediaFlowProject/Processing/MediaProcessorResolver.cs
@@ -1,6 +1,24 @@
-﻿namespace MediaFlowProject.Processing;
+﻿using MediaFlowProject.Models;
+
+namespace MediaFlowProject.Processing;
 
 public class MediaProcessorResolver
 {
-    
+    public static IMediaProcessor Resolve(MediaOperationType operationType)
+    {
+        switch (operationType)
+        {
+            case MediaOperationType.Convert:
+                return new MediaConverter();
+                
+            case MediaOperationType.ExtractAudio:
+                return new AudioExtractor();
+                
+            case MediaOperationType.Compress:
+                return new MediaCompressor();
+                
+            default:
+                throw new NotSupportedException($"The operation type {operationType} is not supported.");
+        }
+    }
 }
